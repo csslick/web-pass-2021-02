16940fdc69d66cb65d6c0b7d714e6526afff3ac8
csslick <digitone@naver.com>
2021-02-25 17:15:58 +0900

    slider 구현

--- script/main.js
+++ script/main.js
@@ -17,3 +17,22 @@ $(function(){
     );
     
 }); // end $
+
+
+/* slider */
+$(function(){
+    var top = 0; // slider 위치
+    var duration = 3000; // 인터벌 시간
+        
+    setInterval(function(){
+       // 마지막 이미지 위치 확인
+       if(top <= -900) {
+           top = 0; // 처음 이미지 위치
+       }
+       $('.sliders').animate({
+          marginTop: top + 'px' 
+       });        
+        top = top - 300;
+    }, duration);
+    
+});
